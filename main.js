30af260957330c1c8ba5e4581aa6a5b44f9c1a18
massimopallera <pallera.massimo@gmail.com>
2024-10-28 13:49:13 +0100

    logs for elements and pin

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 console.log("Hello node js")
 
+//using _ and caps lock to highlitght that they're vars from terminal
+const _PIN = process.env.PIN;
+
+
+console.log(`Argv elements: ${process.argv.slice(2)}`);
+
+console.log(`PIN: ${_PIN}`);
