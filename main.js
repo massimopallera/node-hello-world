4ba215d514f28a0cfb443af2a593b44e05839ee3
massimopallera <pallera.massimo@gmail.com>
2024-10-28 14:06:04 +0100

    bonus done

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 console.log("Hello node js")
 
+
+const adminPin = 666;
 //using _ and caps lock to highlitght that they're vars from terminal
 const _PIN = process.env.PIN;
 
@@ -7,3 +9,9 @@ const _PIN = process.env.PIN;
 console.log(`Argv elements: ${process.argv.slice(2)}`);
 
 console.log(`PIN: ${_PIN}`);
+
+if (_PIN == adminPin){
+	console.log("Welcome Admin");
+} else{
+	console.log("Access Restriced");
+}
